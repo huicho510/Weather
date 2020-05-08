0aebe3d40fd2685d3dd178681903e214e69d75b6
Luis Cervantes <luiscerv1229@gmail.com>
2020-05-08 16:44:28 -0700

    stuck on forecast

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,95 @@
+let savBtn = document.getElementById("save");
+let textIn = document.getElementById("newItem");
 var APIKey = "166a433c57516f51dfab1f7edaed8413";
-var queryURL =
-  "https://api.openweathermap.org/data/2.5/weather?" +
-  "q=Bujumbura,Burundi&appid=" +
-  APIKey;
+
+// var queryURL =
+//   "https://api.openweathermap.org/data/2.5/weather?" + "q=&appid=" + APIKey;
+
+$("#save").on("click", function (event) {
+  event.preventDefault();
+  var cityName = $('#newItem').val();
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName + "&appid="+ APIKey;
+  
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    $("#info").text(JSON.stringify(response));
+
+    $("#info").html("<h2> " + response.name + ' ' +  "Weather Details </h2>");
+
+    $("#info").append("<li> Wind Speed: " + response.wind.speed + "</li>");
+    $("#info").append("<li> Humidity: " + response.main.humidity + "</li>");
+
+    var tempF = (response.main.temp - 273.15) * 1.8 + 32;
+
+    // $("#info").append("Temperature (K) " + response.main.temp);
+    $("#info").append("<li>Temperature: " + tempF.toFixed(2) +  "(F)</li>");
+
+
+  });
+});
+
+function addCity() {
+  $("#newItem").keyup(function (e) {
+    var code = e.which;
+    if (code == 13) {
+      e.preventDefault();
+      $("ul").append("<li>" + e.target.value + "</li>"); // new hw might be useful
+    }
+  });
+}
+
+function myFunction() {
+  savBtn.keyup = $("ul").append("<li>" + textIn.value + "</li>");
+  
+}
+$("#save").on("click", function (event) {
+  event.preventDefault();
+var cityName = $('#newItem').val();
+var weeklyUrl ="api.openweathermap.org/data/2.5/forecast?q="+ cityName +"&appid="+ APIKey
+
 
 $.ajax({
-  url: queryURL,
+  url: weeklyUrl,
   method: "GET",
-})
-.then(function (response) {
-  console.log(queryURL);
+}).then(function (response) {
+  $("#week").text(JSON.stringify(response));
 
-  console.log(response);
+  $("#week").html("<h2> " + response.name + ' ' +  "Weather Details </h2>");
 
-  $("#info").html("<h1>" + response.name + " Weather Details</h1>");
-
-  $("#info").append("<li> Wind Speed: " + response.wind.speed + "</li>");
-  $("#info").append("<li> Humidity: " + response.main.humidity + "</li>");
+  $("#card").append("<li> Humidity: " + response.list.main.humidity + "</li>");
 
   var tempF = (response.main.temp - 273.15) * 1.8 + 32;
 
-  $("#info").append("Temperature (K) " + response.main.temp);
-  $("#info").append("Temperature (F) " + tempF.toFixed(2));
-});
+  $("#th").append("<li>Temperature: " + tempF.toFixed(2) + "(F)</li>");
 
-$("#newItem").keyup(function (e) {
-  var code = e.which;
-  if (code == 13) {
-    e.preventDefault();
-    $("ul").append("<li>" + e.target.value + "</li>"); // new hw might be useful
-  }
+
 });
+});
+// $("#save").on("click", function (event) {
+//   event.preventDefault();
+//   var cityName = $('#newItem').val();
+//   var queryURL = "api.openweathermap.org/data/2.5/onecall?uvi?q="+ cityName + "&appid="+ APIKey;
+
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET",
+//   }).then(function (response) {
+//     $("#info").text(JSON.stringify(response));
+
+//     $("#info").append("<li> " + response.current.uvi + "</li>");
+
+//   });
+// });
+
+
+
+  
+
+
+//
+
+addCity();
+savBtn.addEventListener("click", myFunction);
